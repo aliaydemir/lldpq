0b885a385e22e38959163514ed6fe165a64b7c3b
aliaydemir <alisuayb@gmail.com>
2025-08-07 15:02:09 +0300

    NVIS

--- monitor/monitor.sh
+++ monitor/monitor.sh
@@ -164,7 +164,6 @@ EOF
         done
 
         echo "<h1></h1><h1><font color=\"#b57614\">VLAN Configuration Table '"$hostname"'</font></h1><h3></h3>"
-#        echo "<pre style=\"font-family:monospace;\">"
         printf "<span style=\"color:green;\">%-20s %-12s %s</span>\n" "PORT" "PVID" "VLANs"
         /usr/sbin/bridge vlan | \
           awk '\''BEGIN{cp=""}
@@ -186,7 +185,7 @@ EOF
                 }
                 printf "%s|%s|%s|%s\n", n, $1, $2, $3
            }'\'' | \
-          sort -t"|" -k1,1n | \
+          sort -t"|" -k1,1n -k2,2V | \
           awk -F"|" '\''{
                # Apply colors but use fixed-width formatting
                port_name = $2
